1fd3cfe7f40b41a111f083c901a1a474b5a2786b
Stephan Baumeister <stephan@baumeister.name>
2021-11-30 22:00:59 +0100

    preparation

--- src/Day01.kt
+++ src/Day01.kt
@@ -8,10 +8,10 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readLines("Day01_test.txt")
     check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    val input = readLines("Day01.txt")
     println(part1(input))
     println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,13 +1,6 @@
 import java.io.File
-import java.math.BigInteger
-import java.security.MessageDigest
 
-/**
- * Reads lines from the given input txt file.
- */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
+fun readLines(name: String) = File("src", name).readLines()
 
-/**
- * Converts string to md5 hash.
- */
-fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+fun readText(name: String) = File("src", name).readText()
+
