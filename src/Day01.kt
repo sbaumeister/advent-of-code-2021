ec9cee2c1c5720129d52dc6c9aeec52f1c796b2b
Stephan Baumeister <stephan@baumeister.name>
2021-12-01 23:30:08 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,26 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        var countIncrease = 0
+        input.indices.forEach {
+            if (it > 0 && input[it] - input[it - 1] > 0) {
+                countIncrease++
+            }
+        }
+        return countIncrease
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        var countIncrease = 0
+        input.indices.forEach {
+            if (it + 3 < input.size &&
+                input[it] + input[it + 1] + input[it + 2] < input[it + 1] + input[it + 2] + input[it + 3]) {
+                countIncrease++
+            }
+        }
+        return countIncrease
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readLines("Day01_test.txt")
-    check(part1(testInput) == 1)
-
-    val input = readLines("Day01.txt")
+    val input = readLines("Day01.txt").map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
